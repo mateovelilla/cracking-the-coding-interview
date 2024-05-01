dcbdf94fa1e2aaf1741d9c758647b2178fe86ce7
mateovelilla <teovelilla@gmail.com>
2024-04-30 22:21:20 -0500

    space complexity in rust

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # cracking-the-coding-interview
-# cracking-the-coding-interview
-# cracking-the-coding-interview
-# cracking-the-coding-interview
+Repository to practice examples of Cracking The Coding Interview but in Rust language
+
+## Space Complexity (Cap 6)
+
+### Rust
+![Space Complexity In Rust](https://github.com/mateovelilla/crackin-the-coding-interview/blob/main/cap_6/Rust/space_complexity/cart.png?raw=true)
---
+++ cap_6/Rust/space_complexity/src/main.rs
@@ -0,0 +1,26 @@
+use std::time::{Instant};
+use plotly::{Plot, Scatter};
+
+fn sum(num:i32) -> i32{
+    if num == 0 {
+        return 0;
+    }
+    return num + sum(num - 1);
+}
+// this would take O(N) time and O(N) in space.
+fn main() {
+    
+    let mut x:[i32; 5000] = [0;5000];
+    let mut y:[f32; 5000] = [0.0;5000];
+    for i in 0..x.len() {
+        let start = Instant::now();
+        sum(i as i32);
+        let duration = start.elapsed();
+        x[i] = i as i32;
+        y[i] = duration.as_secs_f32();
+    }
+    let mut plot = Plot::new();
+    let trace = Scatter::new(x.to_vec(), y.to_vec());
+    plot.add_trace(trace);
+    plot.write_html("chart.html");
+}
